86c9ce63110e7b9c42bc20ba8222dc571adee648
Auberon Lopez <auberon.lopez@greenriver.edu>
2025-03-30 15:41:43 -0700

    Problem Solving

---
+++ src/ProblemSolving.java
@@ -0,0 +1,55 @@
+import java.util.Set;
+
+public class ProblemSolving {
+
+  /**
+   * Returns whether every word in the set starts with the letter A.
+   * 
+   * Edge case: If set is empty, return true.
+   * 
+   * @param words a set of words
+   * @return true if every word starts with A (case-insensitive), false otherwise.
+   */
+  public static boolean allStartWithA(Set<String> words) {
+    return false;
+  }
+
+  /**
+   * Returns whether there exists at least one empty string in the set of words.
+   * 
+   * Edge case: If set is empty, return false.
+   * 
+   * @param words a set of words
+   * @return true if there is at least one empty string, false otherwise
+   */
+  public static boolean hasEmptyString(Set<String> words) {
+    return false;
+  }
+
+  /**
+   * Returns the length of the shortest word in the set.
+   * 
+   * Edge case: if set is empty, return Integer.MAX_VALUE
+   * 
+   * @param words a set of words
+   * @return the minimum length of a word in the set
+   */
+  public static int minLength(Set<String> words) {
+    return Integer.MAX_VALUE;
+  }
+
+  /**
+   * Returns the length of the longest word in the set.
+   * 
+   * Edge case: If the set is empty, return 0
+   * 
+   * @param words a set of words
+   * @return the maximum length of a word in the set
+   */
+  public static int maxLength(Set<String> words) {
+    return 0;
+  }
+
+
+  
+}
