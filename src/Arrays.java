c42ec66f69a99cd153f036e54283ec26b11fad5b
Auberon López <auberonedu@gmail.com>
2025-03-31 13:02:45 -0700

    Updated tests and arrays

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,6 +1,7 @@
 public class Arrays {
 
-  public static void main(String args) {
+  public static void main(String[] args) {
+    System.out.println("yo");
     // 1D ARRAYS //
     // Create a 1d array of Strings of length 4
 
---
+++ src/ProblemSolvingTest.java
@@ -0,0 +1,78 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+public class ProblemSolvingTest {
+  @Test
+  void testAllStartWithA_trueMixedCase() {
+    // arrange
+    Set<String> input = Set.of("armadillo", "Arcanine", "asbestos", "ABBA");
+    // act
+    boolean actual = ProblemSolving.allStartWithA(input);
+    //assert
+    assertTrue(actual);
+  }
+
+  @Test
+  void testAllStartWithA_falseOneElement() {
+    // arrange
+    Set<String> input = Set.of("armadillo", "utopia", "Arcanine");
+    // act
+    boolean actual = ProblemSolving.allStartWithA(input);
+    // assert
+    assertFalse(actual);
+  }
+
+  // TODO:
+  // Come up with more tests to thoroughly test testAllStartWithA
+
+  @Test
+  void testHasEmptyString_falseAllNonEmpty() {
+    // arrange
+    Set<String> input = Set.of("armadillo", "Arcanine", "utopia");
+    // act
+    boolean actual = ProblemSolving.hasEmptyString(input);
+    // assert
+    assertFalse(actual);
+  }
+
+  @Test
+  void testHasEmptyString_trueOneEmpty() {
+    // arrange
+    Set<String> input = Set.of("armadillo", "", "utopia", "yeah");
+    // act
+    boolean actual = ProblemSolving.hasEmptyString(input);
+    // assert
+    assertTrue(actual);
+  }
+
+
+  // TODO:
+  // Come up with more tests to thoroughly test hasEmptyString
+
+
+  @Test
+  void testMaxLength_multipleWords() {
+    // arrange
+    Set<String> input = Set.of("by", "a", "commodius", "vicus");
+    // act
+    int actual = ProblemSolving.maxLength(input);
+    // assert
+    assertEquals(9, actual);
+  }
+
+
+  // TODO:
+  // Come up with more tests to thoroughly test maxLength
+
+  @Test
+  void testMinLength() {
+    // TODO:
+    // Come up with ALL tests to thoroughly test minLength
+  }
+
+}
